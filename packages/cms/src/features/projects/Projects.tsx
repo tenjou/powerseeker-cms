b8e2e41e5a382ebe7555855aadc2c0744c1d4409
Tenjou <sefers.arturs@gmail.com>
2020-11-21 13:48:20 +0200

    update: complete Projects page rendering

--- packages/cms/src/features/projects/Projects.tsx
+++ packages/cms/src/features/projects/Projects.tsx
@@ -1,36 +1,62 @@
 import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import { useHistory } from "react-router-dom"
 import { RootState } from "../../app/RootReducer"
 import { Project } from "../../Types"
 import * as ProjectsSlice from "./ProjectsSlice"
 
+const ProjectsHeader = () => {
+    const dispatch = useDispatch()
+
+    const handleCreate = () => {
+        dispatch(ProjectsSlice.create())
+    }
+
+    return (
+        <div>
+            <h1>Projects</h1>
+            <button onClick={handleCreate}>Create</button>
+        </div>
+    )
+}
+
 export default function Projects() {
     const dispatch = useDispatch()
+    const history = useHistory()
     const projects = useSelector((state: RootState) => state.projects)
+    const projectsIds = Object.keys(projects)
 
     useEffect(() => {
         dispatch(ProjectsSlice.load())
     }, [])
 
-    const handleCreate = () => {
-        dispatch(ProjectsSlice.create())
+    const handleOpen = (projectId: string) => {
+        history.push(`/project/${projectId}`)
     }
 
     const handleRemove = (project: Project) => {
         dispatch(ProjectsSlice.remove(project))
     }
 
+    if (projectsIds.length === 0) {
+        return (
+            <>
+                <ProjectsHeader />
+                <div>No projects</div>
+            </>
+        )
+    }
+
     return (
         <>
-            <div>
-                <h1>Projects</h1>
-                <button onClick={handleCreate}>Create</button>
-            </div>
+            <ProjectsHeader />
             <div>
                 <ul>
-                    {Object.keys(projects).map((projectId) => (
+                    {projectsIds.map((projectId) => (
                         <li key={projectId}>
-                            {projects[projectId].meta.name}
+                            <button onClick={() => handleOpen(projectId)}>
+                                {projects[projectId].meta.name}
+                            </button>
                             <button
                                 onClick={() =>
                                     handleRemove(projects[projectId])
