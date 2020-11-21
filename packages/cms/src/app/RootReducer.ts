5efe282fb633fc2e71621413a035f386f86f575d
Tenjou <sefers.arturs@gmail.com>
2020-11-21 03:54:08 +0200

    remove: SheetSlice

--- packages/cms/src/app/RootReducer.ts
+++ packages/cms/src/app/RootReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit"
-import sheet from "../features/sheet/SheetSlice"
 import project from "./../features/project/ProjectSlice"
 import state from "./../features/state/StateSlice"
 
 const rootReducer = combineReducers({
-    sheet,
     project,
     state,
 })
