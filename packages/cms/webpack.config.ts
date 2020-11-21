a47198581ddbab5c193f5c836fd24c5f5e2e5669
Tenjou <sefers.arturs@gmail.com>
2020-11-21 03:53:41 +0200

    fix: webpack dev server default path

--- packages/cms/webpack.config.ts
+++ packages/cms/webpack.config.ts
@@ -6,6 +6,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "public"),
+        publicPath: "/",
     },
     devtool: "source-map",
     mode: "development",
