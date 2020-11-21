284345e5d0025e3a45d830d678c036de0148cf59
Tenjou <sefers.arturs@gmail.com>
2020-11-21 11:03:13 +0200

    add: SchemaSlice.create

--- packages/cms/src/features/schema/SchemaSlice.ts
+++ packages/cms/src/features/schema/SchemaSlice.ts
@@ -1,13 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "../../app/RootReducer"
+import { AppDispatch } from "../../app/Store"
 
-type Schema = {}
+type SchemaKeyType = "string" | "number"
+
+type Schema = {
+    [key: string]: {
+        type: SchemaKeyType
+        default: unknown
+    }
+}
 
 const initialState: Record<string, Schema> = {}
 
 const schemaSlice = createSlice({
     name: "schema",
     initialState,
-    reducers: {},
+    reducers: {
+        update(state, action: PayloadAction<SchemaInput>) {
+            state[action.payload.id] = action.payload.schema
+        },
+    },
 })
+
+export const create = (id: string, schema: Schema) => (
+    dispatch: AppDispatch,
+    getState: () => RootState
+) => {
+    dispatch(
+        schemaSlice.actions.update({
+            id,
+            schema,
+        })
+    )
+}
+
+type SchemaInput = {
+    id: string
+    schema: Schema
+}
 
 export default schemaSlice.reducer
