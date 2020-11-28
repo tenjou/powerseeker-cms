285fdfae7b2af2aa51cf2d65c6c0f611a3d3b5fc
Arturs Šefers <sefers.arturs@gmail.com>
2020-11-28 11:58:47 +0200

    update: NavBar

--- packages/cms/src/components/NavBar.tsx
+++ packages/cms/src/components/NavBar.tsx
@@ -1,5 +1,7 @@
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
+import { RootState } from "../app/RootReducer"
 import { Project } from "../Types"
 
 const NavBarBody = styled.div`
@@ -11,9 +13,11 @@ const NavBarBody = styled.div`
     padding: 0 10px;
 `
 
-const NavTitle = styled.div`
-    font-weight: 500;
+const NavTitle = styled(Link)`
     padding-right: 10px;
+    color: #fff;
+    font-weight: 500;
+    text-decoration: none;
 `
 
 const NavLink = styled(Link)`
@@ -27,9 +31,15 @@ type NavBarProps = {
     project: Project
 }
 const NavBar = ({ project }: NavBarProps) => {
+    const selectedAssetId = useSelector(
+        (state: RootState) => state.cache.selectedAssetId
+    )
+
     return (
         <NavBarBody>
-            <NavTitle>{project.meta.name}</NavTitle>
+            <NavTitle to={`/project/${project.meta.id}/${selectedAssetId}`}>
+                {project.meta.name}
+            </NavTitle>
             <NavLink to="/import">Import</NavLink>
             <NavLink to="/export">Export</NavLink>
         </NavBarBody>
